618be08afd10cbe375f36571714a550fee578298
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2020-10-13 10:44:51 +0200

    commit 2

--- PokladnaFork-DH/PokladnaFork-DH/Form1.cs
+++ PokladnaFork-DH/PokladnaFork-DH/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,14 +9,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PokladnaFork_DH
+namespace Pokladna
 {
     public partial class Form1 : Form
     {
+        List<PokladniZaznam> pokladna;
+        IRepos repositar;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            repositar = new JsonRepos();
+            //repositar = new SqlRepos();
+            //repositar = new XmlRepos();
+            pokladna = repositar.NactiVse();
+        }
     }
 }
