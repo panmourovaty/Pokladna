7718f0b0c6424a957cf5209450705987f92f825e
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2020-10-13 07:11:36 +0200

    first commit

---
+++ PokladnaFork-DH/PokladnaFork-DH/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokladnaFork_DH
+{
+    class PoklZaznam
+    {
+        public int IdPokladniZaznam { get; set; }
+        public int Cislo { get; set; }
+        public DateTime Datum { get; set; }
+        public string Popis { get; set; }
+    }
+}
